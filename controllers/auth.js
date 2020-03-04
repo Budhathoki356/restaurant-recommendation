4f48795bf92a13a7cccfff5758022c426eaaeb8c
madhu <budhathoki1418@gmail.com>
2020-03-04 12:46:02 +0545

    Email is completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,22 @@ var jwt = require('jsonwebtoken');
 var passwordHash = require('password-hash');
 var UserModel = require('../models/user.model');
 var mapUser = require('../helpers/map_user_req');
+var randomString = require('randomstring');
+var mailSender = require('../config/nodemailer.config');
+
+function createMail(data) {
+    let mailOptions = {
+        from: 'From RMS <norply@example.com>',
+        to: `${data.email}`,
+        subject: 'Forget Password',
+        text: 'Forget password?',
+        html: `<p><br> Hi, ${data.name}</br>,</p><p> We noticed that you are having trouble logging into our system, Please click the link below to reset your password </p>
+        <p><a href='${data.link}' target="_blank">Password reset link.</a></p>
+        <p>Regards, </p><p>RMS team.</p>`
+    };
+
+    return mailOptions;
+}
 
 // To GENETRATE TOKEN
 
@@ -92,5 +108,81 @@ router
             })
     });
 
+router.post('/forgotPassword', function (req, res, next) {
+    var email = req.body.email;
+    if (!email) {
+        return res.json({
+            message: 'email not reqgistered yet.'
+        })
+    }
+    UserModel.findOne({ email: email }).exec(function (err, user) {
+        if (err) {
+            return res.status(500).json({
+                error: err
+            })
+        }
+        if (user) {
+            var expiryTime = new Date().getTime() + 1000 * 60 * 60 * 5;
+            var passwordResetToken = randomString.generate(25);
+            var passwordResetTokenExpiry = new Date(expiryTime);
+            var prepareEmail = createMail({
+                name: user.username,
+                email: user.email,
+                link: `${req.headers.origin}/auth/reset-password/${passwordResetToken}`
+            });
+
+            user.passwordResetToken = passwordResetToken;
+            user.passwordResetTokenExpiry = passwordResetTokenExpiry;
+            user.save(function (err, done) {
+                if (err) {
+                    return next(err);
+                }
+                // email pathaune
+                mailSender.sendMail(prepareEmail, function (err, done) {
+                    if (err) {
+                        return next(err);
+                    }
+                    res.json(done);
+                })
+            });
+
+        } else {
+            next({
+                message: 'Email not registered yet'
+            });
+        }
+    })
+});
+
+router.post('/reset-password/:token', function (req, res, next) {
+    UserModel.findOne({
+        passwordResetToken: req.params.token
+    }).exec(function (err, user) {
+        if (err) {
+            return next(err);
+        }
+        if (user) {
+            if (new Date(user.passwordResetTokenExpiry).getTime() > Date.now()) {
+                user.password = passwordHash.generate(req.body.password);
+                user.passwordResetToken = null;
+                user.passwordResetTokenExpiry = null;
+                user.save(function (err, done) {
+                    if (err) {
+                        return next(err);
+                    }
+                    res.json(done);
+                })
+            } else {
+                next({
+                    message: "password reset link expired"
+                })
+            }
+        } else {
+            next({
+                message: "token expired or invalid token"
+            });
+        }
+    })
+});
 
 module.exports = router;
