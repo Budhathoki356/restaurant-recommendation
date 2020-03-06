d86e56f4db670848326890b2814068958766b395
madhu <budhathoki1418@gmail.com>
2020-03-06 13:47:46 +0545

    user Profile is completed.

--- controllers/users.js
+++ controllers/users.js
@@ -38,6 +38,82 @@ var upload = multer({
     storage: storage,
     fileFilter: fileFilter
 });
+/**
+ * Routes for User Profile
+ */
+
+router.route('/profile/:id')
+    .get(function (req, res, next) {
+        var userid = req.params.id;
+        UserModel.findById({ _id: userid }).exec(function (err, user) {
+            if (err) return res.status(500).json({
+                error: err
+            });
+            if (user) {
+                return res.status(200).json(user);
+            } else {
+                return res.status(404).json({
+                    message: 'User not found.'
+                })
+            }
+        })
+    })
+    .put(function (req, res, next) {
+        var userId = req.params.id;
+        UserModel.findById({ _id: userId })
+            .exec()
+            .then(user => {
+                if (user) {
+                    var updatedUser = mapUser(user, req.body);
+                    if (req.body.password) {
+                        updatedUser.password = passwordHash.generate(req.body.password);
+                    }
+                    updatedUser.save(function (err, done) {
+                        if (err) {
+                            return res.status(500).json({
+                                error: err
+                            });
+                        }
+                        res.status(200).json(done);
+                    });
+                } else {
+                    return res.status(404).json({
+                        message: 'User not found.'
+                    });
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error: err,
+                })
+            })
+    })
+    .delete(function (req, res, next) {
+        var userId = req.params.id;
+        UserModel.findOne({ _id: userId })
+            .exec(function (err, user) {
+                if (err) {
+                    return res.status(500).json({
+                        error: err
+                    });
+                }
+                if (user) {
+                    // delete selected user
+                    user.remove(function (err, done) {
+                        if (err)
+                            return res.status(500).json({
+                                error: err
+                            });
+                        return res.status(200).json(done);
+                    })
+                } else {
+                    return res.status(404).json({
+                        message: 'User not found',
+                    })
+                }
+            });
+    })
 
 /**
  * Routes for fooditem
@@ -160,7 +236,7 @@ router.route('/foodItem/:id')
 
 
 router.route('/')
-    .get(function (req, res, next) {
+    .get(authorize, function (req, res, next) {
         UserModel.find({})
             .exec(function (err, users) {
                 if (err) {
@@ -171,7 +247,7 @@ router.route('/')
                 return res.status(200).json(users);
             })
     })
-    .post(authorize,function (req, res, next) {
+    .post(authorize, function (req, res, next) {
         var newUser = new UserModel({});
         var newMappedUser = mapUser(newUser, req.body);
         if (req.body.password) {
@@ -186,7 +262,7 @@ router.route('/')
             return res.status(200).json(user);
         })
     });
-    
+
 /**
  *  Get user by Id
  */
@@ -209,7 +285,7 @@ router.route('/:id')
     })
 
     // Update User By Id
-    .put(function (req, res, next) {
+    .put(authorize, function (req, res, next) {
         var userId = req.params.id;
         UserModel.findById({ _id: userId })
             .exec()
